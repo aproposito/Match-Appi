ebcc56ecf5f2055be13dfbbd78d00a885c9842e7
Alvaro <aproposito@gmail.com>
2026-06-08 13:28:58 +0200

    feat: GET /users endpoint (admin only)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,8 +11,11 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    
     Route::get('/user', function (Request $request) {
         return $request->user();
+    });
+
+    Route::middleware('admin')->group(function () {
+        Route::get('/users', [UserController::class, 'index']);
     });
 });
